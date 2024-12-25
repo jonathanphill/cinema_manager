ca973618536aaf327e4d4808ec4a2b1223ef4bd7
jonathanphill <jonathanphill@gmail.com>
2024-12-25 11:00:17 -0500

    "Finally Solved Cinema Room Manager"

--- Cinema Room Manager (Kotlin)/task/src/cinema/Cinema.kt
+++ Cinema Room Manager (Kotlin)/task/src/cinema/Cinema.kt
@@ -20,6 +20,11 @@ fun main() {
     var userSelection: Int?
     var userSelectedRow = 0
     var userSelectedSeat = 0
+    var numberOfTickets = 0
+    var ticketsSoldAtTen =0
+    var tickersSoldAtEight = 0
+    var currentIncome = 0
+
     do {
         userSelection = readlnOrNull()?.toIntOrNull()
 
@@ -31,29 +36,61 @@ fun main() {
             }
 
             2 -> {
-                println("Enter a row number:")
-                userSelectedRow = readlnOrNull()?.toIntOrNull() ?: continue
-                println("Enter a seat number in that row:")
-                userSelectedSeat = readlnOrNull()?.toIntOrNull() ?: continue
+                var validSelection = false
+                while (!validSelection) {
+                    println()
+                    println("Enter a row number:")
+                    userSelectedRow = readlnOrNull()?.toIntOrNull() ?: continue
+                    println("Enter a seat number in that row:")
+                    userSelectedSeat = readlnOrNull()?.toIntOrNull() ?: continue
 
-                if (userSelectedRow in 1..userInputRows && userSelectedSeat in 1..userInputSeats) {
-                    if (seats[userSelectedRow - 1][userSelectedSeat - 1] == 'S') {
-                        seats[userSelectedRow - 1][userSelectedSeat - 1] = 'B'
-                        val seatsCount = userInputRows * userInputSeats
-                        val ticketPrice = calculateTicketPrice(seatsCount, userInputRows, userSelectedRow)
-                        println("Ticket Price: $$ticketPrice")
+                    if (userSelectedRow in 1..userInputRows && userSelectedSeat in 1..userInputSeats) {
+                        if (seats[userSelectedRow - 1][userSelectedSeat - 1] == 'S') {
+                            seats[userSelectedRow - 1][userSelectedSeat - 1] = 'B'
+                            val seatsCount = userInputRows * userInputSeats
+                            val ticketPrice = calculateTicketPrice(seatsCount, userInputRows, userSelectedRow)
+                            println("Ticket Price: $$ticketPrice")
+                            numberOfTickets++
+                            if(ticketPrice ==10){
+                                ticketsSoldAtTen++
+                            }else{
+                                tickersSoldAtEight ++
+                            }
+                            currentIncome =( ticketsSoldAtTen * 10) + (tickersSoldAtEight * 8)
+                            validSelection = true
+
+                        } else {
+                            println("That ticket has already been purchased!")
+                            println()
+
+                        }
                     } else {
-                        println("This seat is already booked.")
+                        println("Wrong input!")
+                        println()
+
                     }
-                } else {
-                    println("Invalid seat selection.")
                 }
                 println()
                 showMenu()
             }
 
+            3 -> {
+                var totalIncome = calculateTotalIncome(userInputRows, userInputSeats)
+                var totalSeats = userInputSeats * userInputRows
+                val percentage = ((numberOfTickets / totalSeats.toDouble()) * 100)
+                val formatPercentage = "%.2f".format(percentage)
+                println()
+                println("Number of purchased tickets: $numberOfTickets")
+                println("Percentage: $formatPercentage%")
+                println("Current income: $$currentIncome")
+                println("Total income: $$totalIncome")
+                println()
+                showMenu()
+
+            }
+
             0 -> break
-            else -> println("Please enter 1, 2, or 0.")
+            else -> println("Please enter 1, 2, 3 or 0.")
         }
     } while (true)
 }
@@ -87,22 +124,24 @@ fun calculateTicketPrice(seatsCount: Int, rows: Int, selectedRow: Int): Int {
 fun showMenu() {
     println("1. Show the seats ")
     println("2. Buy a ticket")
+    println("3. Statistics")
     println("0. Exit")
-    println()
+
 }
 
-//if (userInputRows != null && userInputSeats != null && userInputRows <= 9 && userInputSeats <= 9) {
-//    val seatsCount = userInputRows * userInputSeats
-//    seatArrangement(userInputRows, userInputSeats)
-//
-//    println("Enter a row number:")
-//    val userSelectedRow = readlnOrNull()?.toIntOrNull()
-//    println("Enter a seat number in that row:")
-//    val userSelectedSeat = readlnOrNull()?.toIntOrNull()
-//
-//    if (userSelectedRow != null && userSelectedSeat != null) {
-//        val ticketPrice = calculateTicketPrice(seatsCount, userInputRows, userSelectedRow)
-//        println("Ticket Price: $$ticketPrice")
-//        seatArrangement(userInputRows, userInputSeats, userSelectedRow, userSelectedSeat)
-//    }
-//}
+fun calculateTotalIncome(userInputRows: Int, userInputSeats: Int): Int {
+    var totalIncome: Int
+    val seatsCount = userInputRows * userInputSeats
+    totalIncome = if (seatsCount <= 60) {
+        seatsCount * 10
+    } else {
+        val firstHalf = userInputRows / 2
+        val secondHalf = userInputRows - firstHalf
+        (firstHalf * userInputSeats * 10) + (secondHalf * userInputSeats * 8)
+    }
+    return totalIncome
+
+}
+
+
+
